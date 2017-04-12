85d75035192d401223c496d72ced4b1ba7c0f6e2
Marcin Kołda <marcin.kolda@gmail.com>
2017-04-12 22:43:56 +0200

    added dumping projects IAMs

--- collector.py
+++ collector.py
@@ -20,6 +20,21 @@ def dump_projects(iam_iterator):
                              project['createTime'], project['name']])
 
 
+def dump_projects_iam(iam_iterator):
+    with open('projects_iam.csv', 'wb') as csvfile:
+        writer = csv.writer(csvfile)
+        for counter, project in enumerate(iam_iterator.list_projects()):
+            project_id = project['projectId']
+            logging.info("parsing project [{0}] projectId: {1}"
+                         .format(counter, project_id))
+            for binding in iam_iterator.list_project_iam(project_id):
+                role = binding['role']
+                for member in binding['members']:
+                    member_type = member.split(":")[0]
+                    member_name = member.split(":")[1]
+                    writer.writerow(
+                        [project_id, role, member_type, member_name])
+
 def dump_service_accounts(iam_iterator):
     with open('serviceAccounts.csv', 'wb') as csvfile:
         writer = csv.writer(csvfile)
@@ -40,4 +55,5 @@ def dump_service_accounts(iam_iterator):
 if __name__ == '__main__':
     iam_iterator = GcpIamIterator()
     dump_projects(iam_iterator)
+    dump_projects_iam(iam_iterator)
     dump_service_accounts(iam_iterator)
--- gcp_iam_iterator.py
+++ gcp_iam_iterator.py
@@ -2,7 +2,7 @@ import logging
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-from cache_service import CRMProjects, ServiceAccountService, \
+from cache_service import CRMProjects, CRMProjectIam, ServiceAccountService, \
     ServiceAccountKeyService
 
 from oauth2client.client import GoogleCredentials
@@ -11,8 +11,9 @@ from oauth2client.client import GoogleCredentials
 class GcpIamIterator:
     def __init__(self):
         credentials = GoogleCredentials.get_application_default()
-        self.crm_service = CRMProjects(build('cloudresourcemanager', 'v1',
-                                             credentials=credentials))
+        google_crm_service = build('cloudresourcemanager', 'v1', credentials=credentials)
+        self.crm_service = CRMProjects(google_crm_service)
+        self.crm_iam_service = CRMProjectIam(google_crm_service)
         google_iam_service = build('iam', 'v1', credentials=credentials)
         self.sa_service = ServiceAccountService(google_iam_service)
         self.sak_service = ServiceAccountKeyService(google_iam_service)
@@ -29,6 +30,21 @@ class GcpIamIterator:
                     nextPageToken=response['nextPageToken'])
             else:
                 response = None
+
+    def list_project_iam(self, project_id):
+        try:
+            iam_policy = self.crm_iam_service.get(project_id=project_id)
+        except HttpError as e:
+            if e.resp.status == 403:
+                logging.warning("403 received for project_id: {0}"
+                                .format(project_id))
+                return
+            else:
+                raise e
+
+        if 'bindings' in iam_policy:
+            for binding in iam_policy['bindings']:
+                yield binding
 
     def list_service_accounts(self, project_id):
         try:
